075c733db5cb8536c5e23acd974ff7e1cc003e6f
Michał Grabkowski <michal.grabkowski@gmail.com>
2016-10-27 23:09:18 +0200

    NUnit TestFixture base class

---
+++ AutoMocker/src/AutoMocker.NUnit/TestFixtureBase.cs
@@ -0,0 +1,37 @@
+﻿using Moq;
+using NUnit.Framework;
+
+namespace AutoMocker.NUnit
+{
+    public class TestFixtureBase<T>
+    {
+        private SutFactory<T> _sutFactory;
+
+        [SetUp]
+        public virtual void InitTest()
+        {
+            _sutFactory = new SutFactory<T>();
+        }
+
+        [TearDown]
+        public virtual void CleanupTest()
+        {
+            _sutFactory.Dispose();
+        }
+
+        public T Create()
+        {
+            return _sutFactory.Create();
+        }
+
+        public Mock<TDependency> GetMock<TDependency>() where TDependency : class
+        {
+            return _sutFactory.GetMock<TDependency>();
+        }
+
+        public void UseDependency<TDependency>(TDependency dependency) where TDependency : class
+        {
+            _sutFactory.UseDependency(dependency);
+        }
+    }
+}
